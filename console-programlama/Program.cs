1a46970da0d531c1e1ed9d62a93f6c9878e4d868
Oğuz DEMİR <47930174+mechanicworld@users.noreply.github.com>
2021-12-17 16:45:07 +0300

    Add files via upload

---
+++ console-programlama/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Isminizi giriniz");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadinizi giriniz");
+            string surName = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name +" " + surName);
+        }
+    }
+}
