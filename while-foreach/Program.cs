b4fd1e5b53d1f2584c40dc663bb07a1eb4afd4a6
Oğuz DEMİR <47930174+mechanicworld@users.noreply.github.com>
2021-12-21 14:15:24 +0300

    Add  while-foreach

---
+++ while-foreach/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While 
+
+            Console.WriteLine("Enter a number");
+            int number = Int32.Parse(Console.ReadLine());
+            int counter = 1;
+            int sum = 0 ;
+            while(counter <= number)
+            {
+                sum += counter;
+                counter ++;
+
+            }
+            Console.WriteLine(sum/number);
+
+            char character = 'a';
+            while (character < 'z'){
+                Console.WriteLine(character);
+                character ++;
+            }
+
+            Console.WriteLine("Foreach");
+            string[] cars = {"BMW", "Ford", "Toyota", "Nissan"};
+            foreach(var item in cars)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        
+    }
+}
