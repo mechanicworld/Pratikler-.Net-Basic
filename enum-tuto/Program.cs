f031e8551154761e6c98877cd5a200650bb2f202
Oğuz DEMİR <47930174+mechanicworld@users.noreply.github.com>
2021-12-24 15:38:58 +0300

    Add Enum

---
+++ enum-tuto/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace enum_tuto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Days.Friday);
+            Console.WriteLine(Days.Monday);
+            Console.WriteLine(Days.Tuesday);
+            Console.WriteLine((int) Days.Friday);
+
+            int weatherState = 32;
+
+            if(weatherState < (int)Weather.Normal){
+                Console.WriteLine("A litt");
+            }
+        }
+
+        enum Days{
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        enum Weather{
+            Cold = 5,
+            Normal = 20,
+            Hot = 26,
+            TooHot = 32
+        }
+    }
+}
